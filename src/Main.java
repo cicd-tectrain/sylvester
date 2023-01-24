371dff954046486c114f2a1c6810a8962193c953
Sylvester Malanda <sm_miya@yahoo.com>
2023-01-24 12:49:35 +0100

    Update main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Bye world!");
+        System.out.println("Some more output!");
+    }
+}
